5ea73f71d585993c40886677b78e26993a723477
David Rimar <david.rimar@digital.education.gov.uk>
2023-10-25 14:56:08 +0100

    EC-217 add Startup file

--- src/SFA.DAS.EarlyConnect.Api/Program.cs
+++ src/SFA.DAS.EarlyConnect.Api/Program.cs
@@ -10,100 +10,23 @@ using SFA.DAS.EarlyConnect.Data;
 using SFA.DAS.EarlyConnect.Domain.Configuration;
 using System.Text.Json.Serialization;
 using SFA.DAS.Api.Common.AppStart;
+using SFA.DAS.EarlyConnect.Api;
+using System.Reflection.Metadata;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var rootConfiguration = builder.Configuration.LoadConfiguration();
-builder.Services.AddOptions();
-builder.Services.Configure<EarlyConnectConfiguration>(rootConfiguration.GetSection(nameof(EarlyConnectConfiguration)));
-builder.Services.AddSingleton(cfg => cfg.GetService<IOptions<EarlyConnectConfiguration>>().Value);
-builder.Services.AddSingleton(new EnvironmentConfiguration(rootConfiguration["EnvironmentName"]));
-
-var earlyConnectConfiguration = rootConfiguration
-    .GetSection(nameof(EarlyConnectConfiguration))
-    .Get<EarlyConnectConfiguration>();
-
-builder.Services.AddDatabaseRegistration(earlyConnectConfiguration, rootConfiguration["EnvironmentName"]);
-builder.Services.AddMediatR(options => options.RegisterServicesFromAssemblyContaining<DummyQuery>());
-
-builder.Services.AddHealthChecks()
-    .AddDbContextCheck<EarlyConnectDataContext>();
-
-
-builder.Services.AddLogging(config =>
+namespace SFA.DAS.EarlyConnect.Api
 {
-    config.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-    config.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
-});
-
-if (!(rootConfiguration["EnvironmentName"]!.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase) ||
-      rootConfiguration["EnvironmentName"]!.Equals("DEV", StringComparison.CurrentCultureIgnoreCase)))
-{
-    var azureAdConfiguration = rootConfiguration
-        .GetSection("AzureAd")
-        .Get<AzureActiveDirectoryConfiguration>();
-
-    var policies = new Dictionary<string, string>
+    public class Program
     {
-        {PolicyNames.Default, RoleNames.Default},
-    };
-    builder.Services.AddAuthentication(azureAdConfiguration, policies);
-}
-
-builder.Services
-    .AddMvc(o =>
-    {
-        if (!(rootConfiguration["EnvironmentName"]!.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase) ||
-              rootConfiguration["EnvironmentName"]!.Equals("DEV", StringComparison.CurrentCultureIgnoreCase)))
+        public static void Main(string[] args)
         {
-            o.Conventions.Add(new AuthorizeControllerModelConvention(new List<string>()));
+            CreateHostBuilder(args).Build().Run();
         }
-        o.Conventions.Add(new ApiExplorerGroupPerVersionConvention());
-    })
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
 
-builder.Services.AddApplicationInsightsTelemetry();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EarlyConnectApi", Version = "v1" });
-    c.OperationFilter<SwaggerVersionHeaderFilter>();
-});
-
-builder.Services.AddApiVersioning(opt => {
-    opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
-});
-
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EarlyConnectApi v1");
-    c.RoutePrefix = string.Empty;
-});
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder
+                    .UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseAuthentication();
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
-
-if (!app.Configuration["EnvironmentName"]!.Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
-{
-    app.UseHealthChecks();
-}
-
-app.UseEndpoints(config =>
-{
-    config.MapControllerRoute(name: "default", pattern: "api/{controller=Users}/{action=Index}/{id?}");
-});
-
-app.Run();
---
+++ src/SFA.DAS.EarlyConnect.Api/Startup.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Extensions.Options;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.EarlyConnect.Api.AppStart;
+using SFA.DAS.EarlyConnect.Application.Queries;
+using SFA.DAS.EarlyConnect.Data;
+using SFA.DAS.EarlyConnect.Domain.Configuration;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
+using SFA.DAS.Api.Common.Configuration;
+using SFA.DAS.Api.Common.Infrastructure;
+using System.Text.Json.Serialization;
+using SFA.DAS.Api.Common.AppStart;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
+namespace SFA.DAS.EarlyConnect.Api
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration.LoadConfiguration();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<EarlyConnectConfiguration>(_configuration.GetSection(nameof(EarlyConnectConfiguration)));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<EarlyConnectConfiguration>>().Value);
+            services.AddSingleton(new EnvironmentConfiguration(_configuration["EnvironmentName"]));
+
+            var earlyConnectConfiguration = _configuration
+                .GetSection(nameof(EarlyConnectConfiguration))
+                .Get<EarlyConnectConfiguration>();
+
+            services.AddDatabaseRegistration(earlyConnectConfiguration, _configuration["EnvironmentName"]);
+            services.AddMediatR(options => options.RegisterServicesFromAssemblyContaining<DummyQuery>());
+
+            services.AddHealthChecks()
+                .AddDbContextCheck<EarlyConnectDataContext>();
+
+            services.AddLogging(config =>
+            {
+                config.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+                config.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+            });
+
+            if (!ConfigurationIsLocalOrDev())
+            {
+                var azureAdConfiguration = _configuration
+                    .GetSection("AzureAd")
+                    .Get<AzureActiveDirectoryConfiguration>();
+
+                var policies = new Dictionary<string, string>
+                {
+                    {PolicyNames.Default, RoleNames.Default},
+                };
+                services.AddAuthentication(azureAdConfiguration, policies);
+            }
+
+            services
+                .AddMvc(o =>
+                {
+                    if (!ConfigurationIsLocalOrDev())
+                    {
+                        o.Conventions.Add(new AuthorizeControllerModelConvention(new List<string>()));
+                    }
+                    o.Conventions.Add(new ApiExplorerGroupPerVersionConvention());
+                })
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
+
+            services.AddApplicationInsightsTelemetry();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EarlyConnectApi", Version = "v1" });
+                c.OperationFilter<SwaggerVersionHeaderFilter>();
+            });
+
+            services.AddApiVersioning(opt =>
+            {
+                opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EarlyConnectApi v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            if (!_configuration["EnvironmentName"]!.Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+            {
+                app.UseHealthChecks();
+            }
+
+            app.UseEndpoints(config =>
+            {
+                config.MapControllerRoute(name: "default", pattern: "api/{controller=Users}/{action=Index}/{id?}");
+            });
+        }
+
+        private bool ConfigurationIsLocalOrDev()
+        {
+            return _configuration["Environment"].Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase) ||
+                   _configuration["Environment"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
