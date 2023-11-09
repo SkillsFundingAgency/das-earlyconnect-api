31e470c9a64c78bf973fe0ffed725f84f1f977d2
ratheeshri1984 <ratheeshri1984@gmail.com>
2023-11-09 18:18:01 +0000

    EC-211-Update table name changes

--- src/SFA.DAS.EarlyConnect.Database/Tables/ApprenticeMetricsFlagData.sql
+++ src/SFA.DAS.EarlyConnect.Database/Tables/ApprenticeMetricsFlagData.sql
@@ -1,14 +1,14 @@
-﻿CREATE TABLE dbo.ApprenticeMetricsFlag (
+﻿CREATE TABLE dbo.ApprenticeMetricsFlagData (
 	Id				INT NOT NULL PRIMARY KEY IDENTITY,
 	MetricsId		INT NOT NULL ,
 	FlagId			INT NOT NULL ,
 	FlagValue		BIT NOT NULL DEFAULT (0),
 	IsDeleted		BIT NOT NULL DEFAULT (0),
 	DateAdded		DATETIME NOT NULL DEFAULT GETDATE()
-	CONSTRAINT FK_ApprenticeMetricsFlag_ApprenticeMetricsData 
+	CONSTRAINT FK_ApprenticeMetricsFlagData_ApprenticeMetricsData 
         FOREIGN KEY(MetricsId) 
         REFERENCES dbo.ApprenticeMetricsData(Id),
-	CONSTRAINT FK_ApprenticeMetricsFlag_MetricsFlagLookUp 
+	CONSTRAINT FK_ApprenticeMetricsFlagDatag_MetricsFlag 
         FOREIGN KEY(FlagId) 
-        REFERENCES dbo.MetricsFlagLookUp(Id)
+        REFERENCES dbo.MetricsFlag(Id)
 )
