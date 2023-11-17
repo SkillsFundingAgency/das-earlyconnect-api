9ea84de66351b31ee9c435ef331e0435017d5341
ratheeshri1984 <ratheeshri1984@gmail.com>
2023-11-17 11:27:35 +0000

    Master branch config fix

--- src/SFA.DAS.EarlyConnect.Domain/Configuration/EarlyConnectApiConfiguration.cs
+++ src/SFA.DAS.EarlyConnect.Domain/Configuration/EarlyConnectApiConfiguration.cs
@@ -2,18 +2,6 @@
 {
     public class EarlyConnectApiConfiguration
     {
-        public EarlyConnectApi EarlyConnectApi { get; set; }
-        public AzureAd AzureAd { get; set; }
-    }
-
-    public class EarlyConnectApi
-    {
         public string? DatabaseConnectionString { get; set; }
-    }
-
-    public class AzureAd
-    {
-        public string Tenant { get; set; }
-        public string Identifier { get; set; }
     }
 }
