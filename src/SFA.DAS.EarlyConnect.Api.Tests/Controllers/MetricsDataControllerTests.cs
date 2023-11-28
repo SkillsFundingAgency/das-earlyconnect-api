6ab853c0a075a6ed3111a4ebee11a5b07b4ce184
David Rimar <david.rimar@digital.education.gov.uk>
2023-11-28 14:52:19 +0000

    EC-241 fix unit tests

--- src/SFA.DAS.EarlyConnect.Api.Tests/Controllers/MetricsDataControllerTests.cs
+++ src/SFA.DAS.EarlyConnect.Api.Tests/Controllers/MetricsDataControllerTests.cs
@@ -6,6 +6,7 @@ using NUnit.Framework;
 using SFA.DAS.EarlyConnect.Api.Controllers;
 using SFA.DAS.EarlyConnect.Api.Requests.PostRequests;
 using SFA.DAS.EarlyConnect.Application.Commands.CreateMetricsData;
+using SFA.DAS.EarlyConnect.Application.Responses;
 
 
 namespace SFA.DAS.EarlyConnect.Api.Tests.Controllers
@@ -54,6 +55,41 @@ namespace SFA.DAS.EarlyConnect.Api.Tests.Controllers
                    && command.MetricsData.First().NoOfGCSCs.Equals(request.MetricsData.First().NoOfGCSCs)
                    ), It.IsAny<CancellationToken>()),
                 Times.Once);
+        }
+
+        [Test]
+        public async Task POST_MetricsData_InvalidLepsCode_Returns400()
+        {
+            // Arrange
+            var request = _fixture.Create<MetricsDataPostRequest>();
+
+            _mediator.Setup(x => x.Send(It.Is<CreateMetricsDataCommand>(command =>
+                   command.MetricsData.First().Region.Equals(request.MetricsData.First().Region)
+                   && command.MetricsData.First().IntendedStartYear.Equals(request.MetricsData.First().IntendedStartYear)
+                   && command.MetricsData.First().NoOfStudents.Equals(request.MetricsData.First().NoOfStudents)
+                   && command.MetricsData.First().WillingnessToRelocate.Equals(request.MetricsData.First().WillingnessToRelocate)
+                   && command.MetricsData.First().NoOfGCSCs.Equals(request.MetricsData.First().NoOfGCSCs)
+                   ), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(new CreateMetricsDataResponse 
+                    { 
+                        ResultCode = ResponseCode.InvalidRequest,
+                        ValidationErrors = new List<DetailedValidationError>
+                                {
+                                    new DetailedValidationError
+                                    {
+                                        Field = "LepCode", Message = "Cannot get LEPSData by the provided LepCode"
+                                    }
+                                }.Cast<object>().ToList()
+               });
+
+            // Act
+            var actionResult = await _metricsDataController.MetricsData(request);
+            var badRequestResult = actionResult as BadRequestObjectResult;
+
+            // Assert
+            Assert.IsNotNull(actionResult);
+            Assert.IsNotNull(badRequestResult);
+            Assert.That(badRequestResult.StatusCode.Equals(400));
         }
     }
 }
