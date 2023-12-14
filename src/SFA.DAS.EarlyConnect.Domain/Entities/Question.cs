ca415efd156eb77bc37f4fc2c194996fab2fbae9
David Rimar <david.rimar@digital.education.gov.uk>
2023-12-14 17:42:33 +0000

    EC-261 add dbset to context and ef configuration

--- src/SFA.DAS.EarlyConnect.Domain/Entities/Question.cs
+++ src/SFA.DAS.EarlyConnect.Domain/Entities/Question.cs
@@ -7,7 +7,7 @@ namespace SFA.DAS.EarlyConnect.Domain.Entities
         public int Id { get; set; }
         public int SurveyId { get; set; } // FK to Survey
         public Survey Survey { get; set; } // Nav property to Survey
-        public int QuestionId { get; set; } // FK to QuestionType
+        public int QuestionTypeId { get; set; } // FK to QuestionType
         public QuestionType QuestionType { get; set; } // Nav property to QuestionType
         public string QuestionText { get; set; }
         public string ShortDescription { get; set; }
