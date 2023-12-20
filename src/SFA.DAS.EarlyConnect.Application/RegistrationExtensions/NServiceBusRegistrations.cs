4551481f716394327dc966a63cf207fb21429523
David Rimar <david.rimar@digital.education.gov.uk>
2023-12-20 16:19:51 +0000

    EC-261 NServiceBus registration

---
+++ src/SFA.DAS.EarlyConnect.Application/RegistrationExtensions/NServiceBusRegistrations.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+using SFA.DAS.EarlyConnect.Domain.Configuration;
+using SFA.DAS.Notifications.Messages.Commands;
+using SFA.DAS.NServiceBus.Configuration;
+using SFA.DAS.NServiceBus.Configuration.AzureServiceBus;
+using SFA.DAS.NServiceBus.Configuration.NewtonsoftJsonSerializer;
+using SFA.DAS.NServiceBus.Configuration.NLog;
+using SFA.DAS.NServiceBus.Hosting;
+
+namespace SFA.DAS.EarlyConnect.Application.RegistrationExtensions
+{
+    public static class NServiceBusRegistrations
+    {
+        private const string EndpointName = "SFA.DAS.EarlyConnect.Api";
+
+        public static IServiceCollection AddNServiceBus(this IServiceCollection services)
+        {
+            return services
+               .AddSingleton(p =>
+               {
+                   var sp = services.BuildServiceProvider();
+                   var configuration = sp.GetService<EarlyConnectApiConfiguration>().NServiceBusConfiguration;
+                   var hostingEnvironment = p.GetService<IHostingEnvironment>();
+
+                   var endpointConfiguration = new EndpointConfiguration(EndpointName)
+                       .UseErrorQueue($"{EndpointName}-errors")
+                       .UseLicense(configuration.NServiceBusLicense)
+                       .UseMessageConventions()
+                       .UseNewtonsoftJsonSerializer()
+                       .UseNLogFactory();
+
+                   endpointConfiguration.SendOnly();
+
+                   if (hostingEnvironment.IsDevelopment())
+                   {
+                       endpointConfiguration.UseLearningTransport(s => s.AddRouting());
+                   }
+                   else
+                   {
+                       endpointConfiguration.UseAzureServiceBusTransport(configuration.SharedServiceBusEndpointUrl, s => s.AddRouting());
+                   }
+
+                   var endpoint = Endpoint.Start(endpointConfiguration).GetAwaiter().GetResult();
+
+                   return endpoint;
+               })
+               .AddSingleton<IMessageSession>(s => s.GetService<IEndpointInstance>())
+               .AddHostedService<NServiceBusHostedService>();
+        }
+    }
+
+    public static class RoutingSettingsExtensions
+    {
+        private const string NotificationsMessageHandler = "SFA.DAS.Notifications.MessageHandlers";
+
+        public static void AddRouting(this RoutingSettings routingSettings)
+        {
+            routingSettings.RouteToEndpoint(typeof(SendEmailCommand), NotificationsMessageHandler);
+        }
+    }
+}
