26356f585750d56cfeb0b0ee0239a01ff543d042
David Rimar <david.rimar@digital.education.gov.uk>
2024-01-11 16:24:10 +0000

    EC-331 log validations

--- src/SFA.DAS.EarlyConnect.Api/Requests/PostRequests/LogUpdateRequest.cs
+++ src/SFA.DAS.EarlyConnect.Api/Requests/PostRequests/LogUpdateRequest.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace SFA.DAS.EarlyConnect.Api.Requests.PostRequests
 {
     public class LogUpdateRequest
     {
         public int LogId { get; set; }
+        [RegularExpression(@"^[\w\s]+$", ErrorMessage = "Invalid Status")]
         public string Status { get; set; }
         public string Error { get; set; }
     }
