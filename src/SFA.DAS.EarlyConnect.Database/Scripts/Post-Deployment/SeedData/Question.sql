1010c803412c3f060944800af0384e7acae095a8
Karen Hanna <karen@laveryhanna.com>
2024-02-08 11:55:05 +0000

    Added validation message for relocate question

--- src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
+++ src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
@@ -18,7 +18,7 @@ IF NOT EXISTS (SELECT * FROM [dbo].[Question])
 			(1, 1, 3, N'Enter School or College Name',N'',N'',N'', null, 1, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
 			(2, 1, 1, N'What level of apprenticeship are you interested in?',N'Select all that apply',N'',N'Select levels of apprenticeship you are interested in, or select ''Not sure''', null, 2, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
 			(3, 1, 1, N'Have you applied for any of the following?',N'This includes current and past applications. Select all that apply.',N'',N'Select options you have applied for, or select ''None of the above''', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-			(4, 1, 2, N'Are you prepared to move to another area of the country for a role?',N'',N'',N'', null, 4, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+			(4, 1, 2, N'Are you prepared to move to another area of the country for a role?',N'',N'',N'Select if you would move to another area of the country for an apprenticeship', null, 4, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
 			(5, 1, 1, N'What would you like support with?',N'Select all that apply',N'',N'', null, 5, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime))
 				
 		SET IDENTITY_INSERT [dbo].[Question] OFF
