f5b987f730d948b01b5ab3a9353bd7c5330d77b1
Karen Hanna <karen@laveryhanna.com>
2024-02-12 09:35:21 +0000

    updated Q&A data

--- src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Answer.sql
+++ src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Answer.sql
@@ -13,22 +13,21 @@ Post-Deployment Script Template
 IF NOT EXISTS (SELECT * FROM [dbo].[Answer])
     BEGIN
 		SET IDENTITY_INSERT [dbo].[Answer] ON	
-		INSERT INTO dbo.Answer ([Id],[QuestionId],[AnswerText],[ShortDescription],[SortOrder],[IsActive],[DateAdded])
-			VALUES
-				   (1, 1, N'', N'', 1, 1, getdate()),
-				   (2, 2, N'Intermediate apprenticeships (Level 2)', N'Equal to GCSEs', 1,1 , CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (3, 2, N'Advanced apprenticeships (Level 3)', N'Equal to A levels', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (4, 2, N'Higher apprenticeships (Levels 4-5)', N'Equal to a Higher National Certificate or foundation degree', 3,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (5, 2, N'Degree apprenticeships (Levels 6-7)', N'Equal to an undergraduate or postgraduate degree', 4,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (6, 2, N'Not sure', N'', 5,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (7, 3, N'Apprenticeship', N'', 1,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (8, 3, N'University', N'', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (9, 3, N'None of the above', N'', 3,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (10, 4, N'Yes, for the right apprenticeship', N'', 1,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (12, 4, N'No', N'', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (13, 5, N'Understanding apprenticeships', N'Discuss the types of apprenticeship you could do', 1,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (14, 5, N'Finding apprenticeship opportunities', N'Get help from our team to find an apprenticeship', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-				   (15, 5, N'Applying for an apprenticeship', N'Support with your applications and interviews', 3,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime))
-		SET IDENTITY_INSERT [dbo].[Answer] OFF
+INSERT INTO dbo.Answer ([Id],[QuestionId],[AnswerText],[ShortDescription],[SortOrder],[IsActive],[DateAdded])
+	VALUES
+		   (1, 1, N'Intermediate apprenticeships (Level 2)', N'Equal to GCSEs', 1,1 , CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (2, 1, N'Advanced apprenticeships (Level 3)', N'Equal to A levels', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (3, 1, N'Higher apprenticeships (Levels 4-5)', N'Equal to a Higher National Certificate or foundation degree', 3,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (4, 1, N'Degree apprenticeships (Levels 6-7)', N'Equal to an undergraduate or postgraduate degree', 4,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (5, 1, N'Not sure', N'', 5,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (6, 2, N'Apprenticeship', N'', 1,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (7, 2, N'University', N'', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (8, 2, N'None of the above', N'', 3,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (9, 3, N'Yes, for the right apprenticeship', N'', 1,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (10, 3, N'No', N'', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (11, 4, N'Understanding apprenticeships', N'Discuss the types of apprenticeship you could do', 1,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (12, 4, N'Finding apprenticeship opportunities', N'Get help from our team to find an apprenticeship', 2,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
+		   (13, 4, N'Applying for an apprenticeship', N'Support with your applications and interviews', 3,1, CAST(N'2024-01-17T09:49:39.977' AS DateTime))
+SET IDENTITY_INSERT [dbo].[Answer] OFF
     END
 
--- src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
+++ src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
@@ -13,13 +13,12 @@ Post-Deployment Script Template
 IF NOT EXISTS (SELECT * FROM [dbo].[Question])
     BEGIN
 		SET IDENTITY_INSERT [dbo].[Question] ON
-			INSERT INTO dbo.Question ([Id], [SurveyId], [QuestionTypeId], [QuestionText], [ShortDescription], [SummaryLabel], [ValidationMessage],[DefaultToggleAnswerId], [SortOrder], [IsActive], [DateAdded])
-			VALUES 
-			(1, 1, 3, N'Enter School or College Name',N'',N'',N'', null, 1, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-			(2, 1, 1, N'What level of apprenticeship are you interested in?',N'Select all that apply',N'',N'Select levels of apprenticeship you are interested in, or select ''Not sure''', null, 2, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-			(3, 1, 1, N'Have you applied for any of the following?',N'This includes current and past applications. Select all that apply.',N'',N'Select options you have applied for, or select ''None of the above''', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-			(4, 1, 2, N'Are you prepared to move to another area of the country for a role?',N'',N'',N'Select if you would move to another area of the country for an apprenticeship', null, 4, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime)),
-			(5, 1, 1, N'What would you like support with?',N'Select all that apply',N'',N'', null, 5, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime))
-				
-		SET IDENTITY_INSERT [dbo].[Question] OFF
+	INSERT INTO dbo.Question ([Id], [SurveyId], [QuestionTypeId], [QuestionText], [ShortDescription], [SummaryLabel], [ValidationMessage],[DefaultToggleAnswerId], [SortOrder], [IsActive], [DateAdded], [GroupNUmber], [GroupLabel])
+	VALUES 
+	(1, 1, 1, N'What level of apprenticeship are you interested in?',N'Select all that apply',N'Apprenticeship level',N'Select levels of apprenticeship you are interested in, or select ''Not sure''', 5, 1, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime), 2, 'Support Details'),
+	(2, 1, 1, N'Have you applied for any of the following?',N'This includes current and past applications. Select all that apply.',N'Current and past applications',N'Select options you have applied for, or select ''None of the above''', 8, 2, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
+	(3, 1, 2, N'Are you prepared to move to another area of the country for a role?',N'',N'Working in other areas of the country',N'Select if you would move to another area of England for an apprenticeship', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
+	(4, 1, 1, N'What would you like support with?',N'Select all that apply',N'Support request',N'Select what you would like support with', null, 4, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime), 2, 'Support Details')
+
+SET IDENTITY_INSERT [dbo].[Question] OFF
     END
