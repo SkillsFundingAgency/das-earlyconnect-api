b654fb21520a9d98328c9eb41ceb371d78218d9e
David Rimar <david.rimar@digital.education.gov.uk>
2024-02-13 16:15:36 +0000

    ec-369 question change

--- src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
+++ src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
@@ -17,7 +17,7 @@ IF NOT EXISTS (SELECT * FROM [dbo].[Question])
 	VALUES 
 	(1, 1, 1, N'What level of apprenticeship are you interested in?',N'Select all that apply',N'Apprenticeship level',N'Select levels of apprenticeship you are interested in, or select ''Not sure''', 5, 1, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime), 2, 'Support Details'),
 	(2, 1, 1, N'Have you applied for any of the following?',N'This includes current and past applications. Select all that apply.',N'Current and past applications',N'Select options you have applied for, or select ''None of the above''', 8, 2, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
-	(3, 1, 2, N'Are you prepared to move to another area of the country for a role?',N'',N'Working in other areas of the country',N'Select if you would move to another area of England for an apprenticeship', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
+	(3, 1, 2, N'Would you move to another area of England for an apprenticeship?',N'',N'Working in other areas of the country',N'Select if you would move to another area of England for an apprenticeship', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
 	(4, 1, 1, N'What would you like support with?',N'Select all that apply',N'Support request',N'Select what you would like support with', null, 4, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime), 2, 'Support Details')
 
 SET IDENTITY_INSERT [dbo].[Question] OFF
