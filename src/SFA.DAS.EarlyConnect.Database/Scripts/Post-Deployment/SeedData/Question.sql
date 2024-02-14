31de9a652457200c71ad57459bde215f959e67d8
Karen Hanna <karen@laveryhanna.com>
2024-02-14 16:11:34 +0000

    Updated summary labels for Check your Answers page

--- src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
+++ src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/SeedData/Question.sql
@@ -16,8 +16,8 @@ IF NOT EXISTS (SELECT * FROM [dbo].[Question])
 	INSERT INTO dbo.Question ([Id], [SurveyId], [QuestionTypeId], [QuestionText], [ShortDescription], [SummaryLabel], [ValidationMessage],[DefaultToggleAnswerId], [SortOrder], [IsActive], [DateAdded], [GroupNumber], [GroupLabel])
 	VALUES 
 	(1, 1, 1, N'What level of apprenticeship are you interested in?',N'Select all that apply',N'Apprenticeship level',N'Select levels of apprenticeship you are interested in, or select ''Not sure''', 5, 1, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime), 2, 'Support Details'),
-	(2, 1, 1, N'Have you applied for any of the following?',N'This includes current and past applications. Select all that apply.',N'Current and past applications',N'Select options you have applied for, or select ''None of the above''', 8, 2, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
-	(3, 1, 2, N'Would you move to another area of England for an apprenticeship?',N'',N'Working in other areas of the country',N'Select if you would move to another area of England for an apprenticeship', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
+	(2, 1, 1, N'Have you applied for any of the following?',N'This includes current and past applications. Select all that apply.',N'Where you''ve applied to stay in education',N'Select options you have applied for, or select ''None of the above''', 8, 2, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
+	(3, 1, 2, N'Would you move to another area of England for an apprenticeship?',N'',N'Would move to another area in England',N'Select if you would move to another area of England for an apprenticeship', null, 3, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime),1, 'Education and Preferences'),
 	(4, 1, 1, N'What would you like support with?',N'Select all that apply',N'Support request',N'Select what you would like support with', null, 4, 1, CAST(N'2024-01-17T09:49:39.977' AS DateTime), 2, 'Support Details')
 
 SET IDENTITY_INSERT [dbo].[Question] OFF
