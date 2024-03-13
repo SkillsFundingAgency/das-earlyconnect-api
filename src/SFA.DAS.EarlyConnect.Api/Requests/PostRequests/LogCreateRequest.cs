56dfeb6c299c3176d241c8dd73d29c3bc5b8188a
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-03-13 13:33:18 +0000

    EC-419-student Id and survey Id added to the logs

--- src/SFA.DAS.EarlyConnect.Api/Requests/PostRequests/LogCreateRequest.cs
+++ src/SFA.DAS.EarlyConnect.Api/Requests/PostRequests/LogCreateRequest.cs
@@ -11,7 +11,7 @@ namespace SFA.DAS.EarlyConnect.Api.Requests.PostRequests
 
         public string RequestIP { get; set; }
         public string Payload { get; set; }
-        [RegularExpression(@"^[a-zA-Z0-9.()-]+$", ErrorMessage = "Invalid FileName")]
+        [RegularExpression(@"^[a-zA-Z0-9.()-|]+$", ErrorMessage = "Invalid FileName")]
         public string FileName { get; set; }
         [RegularExpression(@"^[\w\s]+$", ErrorMessage = "Invalid Status")]
         public string Status { get; set; }
