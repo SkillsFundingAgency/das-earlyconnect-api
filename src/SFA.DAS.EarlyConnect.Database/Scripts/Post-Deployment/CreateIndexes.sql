87bc31640d4d0f9b559dcbf9abe03719b836f2ab
Karen Hanna <karen@laveryhanna.com>
2024-04-16 11:43:04 +0100

    index added to LEPSCoverage.LEPSId

--- src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/CreateIndexes.sql
+++ src/SFA.DAS.EarlyConnect.Database/Scripts/Post-Deployment/CreateIndexes.sql
@@ -3,3 +3,9 @@
   BEGIN
     CREATE INDEX IX_LEPSCoverage_Postcode ON [LEPSCoverage] (Postcode)
   END
+
+IF NOT EXISTS (SELECT *  FROM sys.indexes WHERE name='IX_LEPSCoverage_LEPSId'
+AND object_id = OBJECT_ID('dbo.LEPSCoverage'))
+BEGIN
+CREATE INDEX IX_LEPSCoverage_LEPSId ON [LEPSCoverage] (LEPSId)
+END
