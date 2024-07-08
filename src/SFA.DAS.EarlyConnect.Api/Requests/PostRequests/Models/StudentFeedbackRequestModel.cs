654ba457e04306681c0f4bbaee2586775a498e4d
David Rimar <david.rimar@digital.education.gov.uk>
2024-07-08 16:09:37 +0100

    ec-533 revert regex again

--- src/SFA.DAS.EarlyConnect.Api/Requests/PostRequests/Models/StudentFeedbackRequestModel.cs
+++ src/SFA.DAS.EarlyConnect.Api/Requests/PostRequests/Models/StudentFeedbackRequestModel.cs
@@ -14,9 +14,11 @@ namespace SFA.DAS.EarlyConnect.Api.Requests.PostRequests.Models
         [RegularExpression(@"^[\w\s]+$", ErrorMessage = "Invalid Status Update")]
         public string StatusUpdate { get; set; }
 
+        [RegularExpression(@"^[\w\s\@\.\\\/\(\)\[\]-]+$", ErrorMessage = "Invalid Notes")]
         public string Notes { get; set; }
 
         [Required]
+        [RegularExpression(@"^[\w\s\@\.-]+$", ErrorMessage = "Invalid Updated by User")]
         public string UpdatedBy { get; set; }
     }
 }
